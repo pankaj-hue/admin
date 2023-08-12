eaeaa7bbcc3e4ada0c107ef9518e2eab91895eca
Routash Kumar <routashsain@gmail.com>
2023-08-12 10:04:28 +0530

    first commit

---
+++ precache-manifest.fb6de18fabe0b77cc62fa85c20c49e35.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfc09322f04af44c00725958e1bf67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c518d75144919bad5d2",
+    "url": "/static/css/2.a53fb0ff.chunk.css"
+  },
+  {
+    "revision": "9f8f31be87281b1bf064",
+    "url": "/static/css/main.7970783a.chunk.css"
+  },
+  {
+    "revision": "4c518d75144919bad5d2",
+    "url": "/static/js/2.9f044147.chunk.js"
+  },
+  {
+    "revision": "4b9f28eff0198987231f792ec518c7ec",
+    "url": "/static/js/2.9f044147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f6fc1e26435dd03b66",
+    "url": "/static/js/3.2d7fa5ec.chunk.js"
+  },
+  {
+    "revision": "1869316cee50e11a93b9",
+    "url": "/static/js/4.356ee265.chunk.js"
+  },
+  {
+    "revision": "cab893c1a3c1d4a59ec2",
+    "url": "/static/js/5.928ca276.chunk.js"
+  },
+  {
+    "revision": "1674d2eaceea2f925fe2",
+    "url": "/static/js/6.ea4d77e9.chunk.js"
+  },
+  {
+    "revision": "21a27b038cf86e5a1599",
+    "url": "/static/js/7.14e40f9c.chunk.js"
+  },
+  {
+    "revision": "9f8f31be87281b1bf064",
+    "url": "/static/js/main.c1472bbf.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.c1472bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63c6cd3d76694cec10f",
+    "url": "/static/js/runtime-main.9b6300df.js"
+  },
+  {
+    "revision": "cbf9c83a4aea661d4c61e3272e5fe789",
+    "url": "/static/media/avatar-1.cbf9c83a.jpg"
+  },
+  {
+    "revision": "034d02d73fd503ee64391c76941432bc",
+    "url": "/static/media/avatar-2.034d02d7.jpg"
+  },
+  {
+    "revision": "7ce49759d33047eada521f43b0aa3294",
+    "url": "/static/media/avatar-3.7ce49759.jpg"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "cbb1401d069209edffd4ea178d9dcba0",
+    "url": "/static/media/logo.cbb1401d.svg"
+  },
+  {
+    "revision": "922dde69be08fa8b62e97282616e430f",
+    "url": "/static/media/profile.922dde69.webp"
+  }
+]);
